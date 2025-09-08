9c1cfdd07eefb19f74b0fa4bba5dea8f656b1c79
Valentin Sergeev <valentin.sergeev09@gmail.com>
2025-09-08 12:44:57 +0300

    Exeptions

--- src/main/java/experimints/Exceptions.java
+++ src/main/java/experimints/Exceptions.java
@@ -2,6 +2,41 @@ package experimints;
 
 public class Exceptions {
     public static void main(String[] args) {
-        
+
+        String[] strArray = {"str1", "str2", "str3"};
+        uncheckedException(strArray);
+        try {
+            pause();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void uncheckedException(String[] str) {
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(str[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("Created exception!");
+        }
+        System.out.println("Program is working");
+
+    }
+    private static void pause() throws InterruptedException {
+        checkedException();
+        System.out.println("pause 3 sec");
+    }
+    private static void checkedException() throws InterruptedException {
+        Thread.sleep(3000);
+
+    }
+
+    private String first(String s) {
+
+
+        return null;
     }
 }
+
